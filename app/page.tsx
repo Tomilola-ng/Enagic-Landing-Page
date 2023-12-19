807328a6ac220fc39dfbc80e54f006e585391571
Tomilola-ng <tee.o2809@gmail.com>
2023-12-19 08:51:57 +0100

    Fresh

--- app/page.tsx
+++ app/page.tsx
@@ -9,26 +9,28 @@ export default function Home() {
     },
   ];
   return (
-    <main className="">
-      <div className="bg-blue-300">
+    <main className="pb-8">
+      <div className="bg-blue-300 p-6">
         <section className="max-container flex-col md:flex-row flexBetween w-full gap-6 min-h-[60vh] mb-4">
           <h1 className="text-xl md:text-2xl font-semibold md:font-bold">
-            Enagic Kangen Ionized Water Machine in Nigeria
+            Enagic Kangen Ionized Water <br /> Machine in Nigeria
           </h1>
-          <Image
-            src="/enagic.png"
-            width={1568}
-            height={1975}
-            className="max-w-[400px] w-full"
-            alt="Enagic Kangen Ionized Water Machine"
-          />
+          <div className="w-96">
+            <Image
+              src="/enagic.png"
+              width={1568}
+              height={1975}
+              className="w-full"
+              alt="Enagic Kangen Ionized Water Machine"
+            />
+          </div>
         </section>
       </div>
-      <section className="md:w-full max-container mb-4">
+      <section className="md:w-full max-container my-8">
         <h1 className="text-xl md:text-2xl font-semibold md:font-bold w-full text-center mb-2">
           What is the Kangen Enagic Ionized Water Machine used for
         </h1>
-        <p>
+        <p className="md:px-8 mx-auto ">
           It is mainly for transforming your ordinary tap water into healthy,
           fresh tasting alkaline drinking water. By choosing to drink alkaline
           water, you aid your body in returning to a balanced state. Kangen
